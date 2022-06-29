6689b03320a2419ffe49dc130b08f7b1dfc555e2
jenzalkind <jen.zalkind@gmail.com>
2022-06-29 23:36:40 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/tripy/MainActivity.kt
+++ app/src/main/java/com/example/tripy/MainActivity.kt
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.tripy.databinding.ActivityMainBinding
-//testing commit
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 }
